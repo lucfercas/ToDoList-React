b7dd6f20d730ae71202083a325e8d6b82974e49d
Lucien Lin <luciensday@gmail.com>
2024-03-05 08:59:25 +0000

    link to tailwind

--- src/components/ItemDetails.jsx
+++ src/components/ItemDetails.jsx
@@ -1,35 +1,53 @@
 import { useContext } from "react";
 import { ListContext } from "../contexts/ListContext";
-import { Trash } from "react-bootstrap-icons"
-
-
+import { Trash } from "react-bootstrap-icons";
 
 const ItemDetails = ({ item }) => {
-    const { removeTodo, completeToggle, editEnabler, editDoneToggle,importance } = useContext(ListContext)
-    return (
+  const {
+    removeTodo,
+    completeToggle,
+    editEnabler,
+    editDoneToggle,
+    importance,
+  } = useContext(ListContext);
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={item.complete}
+        onClick={() => completeToggle(item.id)}
+      />
+      <select
+        value={item.importanceLevel}
+        onChange={(e) => {
+          importance(item.id, e.target.value);
+        }}
+      >
+        <option value="pleaseSelect">Please select</option>
+        <option value="important">Important</option>
+        <option value="notImportant">Not important</option>
+        <option value="urgent">Urgent</option>
+      </select>
 
-      <li>
-        <input  type="checkbox" checked={item.complete} onClick = {() => completeToggle(item.id)} />
-        <select value={item.importanceLevel} onChange={(e)=>{importance(item.id, e.target.value)}}>
-            <option value="pleaseSelect">Please select</option>
-            <option value="important">Important</option>
-            <option value="notImportant">Not important</option>
-            <option value="urgent">Urgent</option>
-        </select>
-
-        <input className="title" 
-        style={{ textDecoration: item.complete ? "line-through" : "none" }} 
-        value={item.title} 
-        onChange={(e)=>{editEnabler(item.id, e.target.value)}} 
+      <input
+        className="title"
+        style={{ textDecoration: item.complete ? "line-through" : "none" }}
+        value={item.title}
+        onChange={(e) => {
+          editEnabler(item.id, e.target.value);
+        }}
         onBlur={() => editEnabler(item.id, item.title)}
         disabled={!item.editing}
-                   />
-        <button onClick = {() => removeTodo(item.id)}><Trash /></button>
-        
-        <button onClick = {() => editDoneToggle(item.id)} >{item.editing? "Done": "Edit"}</button>
-      </li>
-    )
-   
-}
- 
+      />
+      <button onClick={() => removeTodo(item.id)}>
+        <Trash />
+      </button>
+
+      <button onClick={() => editDoneToggle(item.id)}>
+        {item.editing ? "Done" : "Edit"}
+      </button>
+    </li>
+  );
+};
+
 export default ItemDetails;
--- src/components/Navbar..jsx
+++ src/components/Navbar..jsx
@@ -1,17 +1,14 @@
 import { useContext } from "react";
 import { ListContext } from "../contexts/ListContext";
 
-
 const Navbar = () => {
+  const { items } = useContext(ListContext);
+  return (
+    <div className="navbar">
+      <h1 className="text-l font-bold underline">React ToDo List</h1>
+      <p>Currently you have {items.length} things to do...</p>
+    </div>
+  );
+};
 
-    const {items} = useContext(ListContext)
-    return (
-        <div className="navbar">
-            <h1>React ToDo List</h1>
-            <p>Currently you have {items.length } things to do...</p>
-        </div>
-    );
-
-}
- 
 export default Navbar;
