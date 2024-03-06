039b641bee3048ddc3ddf390d0fbe68db8efc489
Lucien Lin <luciensday@gmail.com>
2024-03-06 15:55:08 +0000

    add database but need connect with server

--- model/tasks.js
+++ model/tasks.js
@@ -6,8 +6,38 @@ function createTask(title) {
     insert_task.run(title);
 }
 
-createTask("Eat a banana")
-const tasks = db.prepare("SELECT * FROM tasks").all();
-console.log(tasks)
+
+
+const readTask = db.prepare(/*sql*/`
+SELECT 
+id, 
+title, 
+complete, 
+editing, 
+importanceLevel
+FROM 
+tasks
+`
+
+
+)
+
+
+
+
+CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    complete INTEGER,
+    editing INTEGER,
+    importanceLevel TEXT
+);
+
+
+
+
+// createTask("Eat a banana")
+// const tasks = db.prepare("SELECT * FROM tasks").all();
+// console.log(tasks)
 
 module.exports = { createTask }
