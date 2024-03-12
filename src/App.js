c23c93c8415ff63ab33a1fea0a50c60c5cdbd260
Lucía (Lucy) Fernandez <121562718+lucfercas@users.noreply.github.com>
2024-03-12 11:07:59 +0000

    connected app to supabase + started displaying db info

--- src/App.js
+++ src/App.js
@@ -1,21 +1,70 @@
 import supabase from "./config/supabaseClient";
 // import Add from "./Add"
+import { useEffect, useState } from "react";
 import ListContextProvider from './contexts/ListContext';
 import Navbar from './components/Navbar.';
 import ListDisplay from "./components/ListDisplay";
 import AddItemFrom from "./components/AddItemForm";
+import { fetchWeatherApi } from "openmeteo";
 
 function App() {
-  console.log(supabase)
+  const [fetchError, setFetchError] = useState(null)
+  const [tasks, setTasks] = useState(null)
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const {data, error} = await supabase
+        .from('tasks')
+        .select()
+
+        if (error) {
+          setFetchError('Could not fetch the tasks')
+          setTasks(null)
+          console.log(error)
+        }
+        if (data) {
+          setTasks(data)
+          setFetchError(null)
+        }
+    }
+    fetchTasks()
+
+  }, [])
+
   return (
+
+
+
+
+
     <div className="App">
       <ListContextProvider>
         <Navbar />
         <ListDisplay />
         <AddItemFrom />
+
+        <div className="page home">
+          {fetchError && (<p>{fetchError}</p>)}
+          {tasks && (
+            <div className="tasks">
+              {tasks.map(task => (
+                <p>{task.title}</p>
+              ))}
+            </div>
+          )}
+
+
+        </div>
+
+
+
       </ListContextProvider>
       
      {/* <Add /> */}
+
+
+
+
     </div>
   );
 }
