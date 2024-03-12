df8cafa713ce55b0774c3217d3c527eff3731c2b
Lucía (Lucy) Fernandez <121562718+lucfercas@users.noreply.github.com>
2024-03-12 12:23:48 +0000

    removed useContext logic and import

--- src/components/Navbar..jsx
+++ src/components/Navbar..jsx
@@ -1,12 +1,11 @@
-import { useContext } from "react";
+
 
 
 const Navbar = () => {
-  const { items } = useContext(ListContext);
   return (
     <div className="navbar">
       <h1 className="text-l font-bold underline">React ToDo List</h1>
-      <p>Currently you have {items.length} things to do...</p>
+      <p>Currently you have things to do...</p>
     </div>
   );
 };
